5b76756e116f0966d8bf60c73ba8571a3f06e52c
renancunha <renancunhafonseca@hotmail.com>
2019-05-13 11:38:53 -0300

    dnn factory

---
+++ src/deep_q_learning/dnn_factory.py
@@ -0,0 +1,18 @@
+import keras
+from keras.layers import Dense
+
+
+def create_dnn(input_length: int, num_hidden_neurons: int,
+               num_hidden_layers: int, output_length: int) -> keras.models:
+    model = keras.models.Sequential()
+    model.add(Dense(num_hidden_neurons, input_dim=input_length,
+                    activation='relu'))
+
+    for i in range(num_hidden_layers):
+        model.add(Dense(num_hidden_neurons, activation="relu"))
+
+    model.add(Dense(output_length, activation="linear"))
+    model.compile(loss="mse", optimizer="adam")
+    return model
+
+
